2846fd173830c401b2063396661f444d8ffdc90e
Eduardo Fernandes <eduardofernandes.dev@gmail.com>
2025-09-03 17:31:00 -0300

    criando paginas do projeto

--- arquivos/cinesync/src/App.jsx
+++ arquivos/cinesync/src/App.jsx
@@ -3,12 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <div className="App">
+    <div className="App">
         <h2>CineSync</h2>
-      </div>
-    </>
+    </div>
   );
 }
 
-export default App
+export default App;
--- arquivos/cinesync/src/main.jsx
+++ arquivos/cinesync/src/main.jsx
@@ -1,20 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './index.css'
-import App from './App.jsx'
+import App from "./App";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
+import Favorite from "./pages/Search";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<Home />} />
           <Route path="movie/:id" element={<Movie />} />
           <Route path="search" element={<Search />} />
+          <Route path="favorite" element={<Favorite />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
