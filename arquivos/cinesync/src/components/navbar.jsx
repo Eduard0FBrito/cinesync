83c4ee2462b0125e8438ede4759bfb1c2de5f61c
Eduardo Fernandes <eduardofernandes.dev@gmail.com>
2025-09-04 17:55:14 -0300

    ajustando navbar

--- arquivos/cinesync/src/components/navbar.jsx
+++ arquivos/cinesync/src/components/navbar.jsx
@@ -4,8 +4,9 @@ const Navbar = () => {
     return (
         <nav id="navbar">
             <h2>
-                <Link to="/">CineSync</Link>
+                <Link to="/">CINE<span>Sync</span></Link>
             </h2>
+            <Link to="/">Home</Link>
             <Link to="/favorite">Favorite</Link>
         </nav>
     );
